0799b3061bf4bfa564a53699a9a49b4311ce88d3
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-08-18 14:49:00 +0000

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,7 +11,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <WelcomeMessage /> 
+    <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
