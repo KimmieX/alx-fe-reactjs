af391eb640d28cecc41db0cd144aa5e3649dd0e9
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-08-18 14:27:24 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -15,6 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <WelcomeMessage />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -29,6 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+    
   )
 }
 
