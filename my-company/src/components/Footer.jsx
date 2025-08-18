a6aa1d7ce76bcbe8a738993de95fcaedc42be356
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-08-18 18:11:58 +0000

    Delete my-company directory

--- my-company/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ padding: '10px', backgroundColor: '#eee', marginTop: '20px' }}>
-      <p>&copy; 2025 My Company. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
